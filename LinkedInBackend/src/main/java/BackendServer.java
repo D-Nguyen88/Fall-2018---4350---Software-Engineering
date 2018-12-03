6a4919ca08f34d16b4346925778a03366e993378
LegMuscleCramp <42978644+LegMuscleCramp@users.noreply.github.com>
2018-12-03 17:15:51 -0500

    current backend code
    getting NoClassDefFoundError: java/sql/Connection when trying to run backend server

---
+++ LinkedInBackend/src/main/java/BackendServer.java
@@ -0,0 +1,13 @@
+import java.rmi.Naming;
+
+public class BackendServer {
+    public static void main(String[] args) {
+        try {
+            BackendServerInterface backendService = new LinkedInBackend();
+            Naming.rebind("BackendService",backendService);
+            System.out.println("Server is up, comrade!");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ LinkedInBackend/src/main/java/Client.java
@@ -0,0 +1,29 @@
+import java.rmi.Naming;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            BackendServerInterface st = (BackendServerInterface) Naming.lookup("rmi://"+ args[0] +"/AddService");
+
+            FrontendQuery frontendQuery = new FrontendQuery();
+            System.out.println("First name:");
+            frontendQuery.setFirstName(scanner.nextLine());
+            System.out.println("Last name:");
+            frontendQuery.setLastName(scanner.nextLine());
+            System.out.println("Industry:");
+            frontendQuery.setIndustry(scanner.nextLine());
+            System.out.println("Position:");
+            frontendQuery.setPosition(scanner.nextLine());
+            System.out.println("State:");
+            frontendQuery.setState(scanner.nextLine());
+
+            // run sendQuery method w/ data fields array as argument
+            String result = st.sendQuery(frontendQuery);
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
